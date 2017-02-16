8002410cba7aa5e097b5a8aaacf3738df45b9dac
SYAvdeev <s.y.avdeev@gmail.com>
2017-02-17 03:15:38 +0700

    moved domain model and layout page, started making view model

---
+++ StudentsWebsite/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentsWebsite.Models;
+
+namespace StudentsWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        UniversityDB univDB = new UniversityDB();
+
+        public ActionResult Index()
+        {
+            return View(univDB.Students);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            univDB.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
