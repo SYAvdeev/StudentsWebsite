27886c2ae90d49ef2f8d8f7acd3473a6dd1e4618
SYAvdeev <s.y.avdeev@gmail.com>
2017-03-30 03:13:44 +0700

    beta

--- StudentsWebsite/Controllers/StudentsController.cs
+++ StudentsWebsite/Controllers/StudentsController.cs
@@ -9,14 +9,39 @@ using System.Web.Mvc;
 //using StudentsWebsite.Data_Access_Layer;
 using StudentsWebsite.Models;
 using PagedList;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using StudentsWebsite.Data_Access_Layer;
+using System.Threading.Tasks;
 
 namespace StudentsWebsite.Controllers
 {
+    [Authorize]
     public class StudentsController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public StudentsController() { }
+
+        public StudentsController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Students
+        [AllowAnonymous]
         public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
             var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
@@ -74,6 +99,7 @@ namespace StudentsWebsite.Controllers
 
 
         // GET: Students/Details/5
+        [AllowAnonymous]
         public ActionResult Details(int? id)
         {
             var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
@@ -91,8 +117,12 @@ namespace StudentsWebsite.Controllers
         }
 
         // GET: Students/Create
+        [Authorize(Roles ="Admin,Professor")]
         public ActionResult Create()
         {
+            var student = new Student();
+            student.Enrollments = new List<Enrollment>();
+            PopulateAssignedProfessors(student);
             return View();
         }
 
@@ -101,20 +131,62 @@ namespace StudentsWebsite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FirstName,LastName,Enrollments")] Student student)
+        [Authorize(Roles = "Admin,Professor")]
+        public async Task<ActionResult> Create( RegisterViewModel registerModel, string[] selectedProfessors, string[] grades)
         {
+            var student = new Student() { FirstName = registerModel.FirstName, LastName = registerModel.LastName };
+            student.Enrollments = new List<Enrollment>();
+
             if (ModelState.IsValid)
             {
+                if (selectedProfessors != null)
+                {
+                    foreach (var professor in selectedProfessors)
+                    {
+                        Enrollment newEnrollment = new Enrollment();
+                        newEnrollment.Student = student;
+                        int i = int.Parse(professor);
+                        newEnrollment.ProfessorID = i;
+                        if (!string.IsNullOrEmpty(grades[i - 1]))
+                            newEnrollment.Grade = int.Parse(grades[i - 1]);
+
+                        student.Enrollments.Add(newEnrollment);
+                    }
+                }
+
+                var user = new ApplicationUser { UserName = registerModel.Login };
+                await UserManager.CreateAsync(user, registerModel.Password);
+                student.User = UserManager.FindByName(registerModel.Login);
                 var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
                 db.Students.Add(student);
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
 
-            return View(student);
+            PopulateAssignedProfessors(student);
+            return RedirectToAction("Index");
+        }
+
+        private void PopulateAssignedProfessors(Student student)
+        {
+            var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+
+            var allProfessors = db.Professors.ToList();
+            var studentsProfessors = new HashSet<int>(student.Enrollments.Select(c => c.ProfessorID));
+            var viewModel = new List<ProfessorAssignedViewModel>();
+            foreach (var professor in allProfessors)
+            {
+                viewModel.Add(new ProfessorAssignedViewModel
+                {
+                    ProfessorID = professor.ID,
+                    Course = professor.Course,
+                    Assigned = studentsProfessors.Contains(professor.ID)
+                });
+            }
+            ViewBag.Professors = viewModel;
         }
 
         // GET: Students/Edit/5
+        [Authorize(Roles = "Admin,Professor")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -123,11 +195,23 @@ namespace StudentsWebsite.Controllers
             }
             var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
             Student student = db.Students.Find(id);
+            ApplicationUser user = student.User;
+            StudentEditViewModel editModel = new StudentEditViewModel()
+            {
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                Login = student.User.UserName,
+                Password = "",
+                ConfirmPassword = "",
+                StudentID = student.ID
+            };
+
             if (student == null)
             {
                 return HttpNotFound();
             }
-            return View(student);
+            PopulateAssignedProfessors(student);
+            return View(editModel);
         }
 
         // POST: Students/Edit/5
@@ -135,19 +219,43 @@ namespace StudentsWebsite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StudentID,FirstName,LastName")] Student student)
+        [Authorize(Roles = "Admin,Professor")]
+        public async Task<ActionResult> Edit(RegisterViewModel registerModel, string[] selectedProfessors, string[] grades, int id)
         {
+            var student = new Student() { FirstName = registerModel.FirstName, LastName = registerModel.LastName };
+            student.Enrollments = new List<Enrollment>();
+
             if (ModelState.IsValid)
             {
+                if (selectedProfessors != null)
+                {
+                    foreach (var professor in selectedProfessors)
+                    {
+                        Enrollment newEnrollment = new Enrollment();
+                        newEnrollment.Student = student;
+                        int i = int.Parse(professor);
+                        newEnrollment.ProfessorID = i;
+                        if (!string.IsNullOrEmpty(grades[i - 1]))
+                            newEnrollment.Grade = int.Parse(grades[i - 1]);
+
+                        student.Enrollments.Add(newEnrollment);
+                    }
+                }
+
+                var user = new ApplicationUser { UserName = registerModel.Login };
+                await UserManager.CreateAsync(user, registerModel.Password);
+                student.User = UserManager.FindByName(registerModel.Login);
                 var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
-                db.Entry(student).State = EntityState.Modified;
+                db.Students.Add(student);
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
-            return View(student);
+
+            PopulateAssignedProfessors(student);
+            return RedirectToAction("Index");
         }
 
         // GET: Students/Delete/5
+        [Authorize(Roles = "Admin,Professor")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -156,6 +264,7 @@ namespace StudentsWebsite.Controllers
             }
             var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
             Student student = db.Students.Find(id);
+            //UserManager.
             if (student == null)
             {
                 return HttpNotFound();
@@ -166,10 +275,15 @@ namespace StudentsWebsite.Controllers
         // POST: Students/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Professor")]
         public ActionResult DeleteConfirmed(int id)
         {
             var db = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
             Student student = db.Students.Find(id);
+            var user = UserManager.FindById(student.UserID);
+            if(user != null)
+                UserManager.Delete(user);
+
             db.Students.Remove(student);
             db.SaveChanges();
             return RedirectToAction("Index");
